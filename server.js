27267324c358e53dbe0dee1b3cae620909010f24
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-09-01 22:34:30 -0400

    final commit

--- server.js
+++ server.js
@@ -34,22 +34,53 @@ app.delete("/pokemon/:indexOfPokemonArray", (req, res)=>{
 
 //UPDATE
 app.put("/pokemon/:indexOfPokemonArray", (req,res)=>{
-  if(req.body.readyToBatle === "on"){
-    req.body.readyToBatle = true
-  } else {
-    req.body.readyToBatle = false
-  }
-  pokemon[req.params.indexOfPokemonArray] = req.body
+    pokemon[req.params.indexOfPokemonArray].name = req.body.name;
+    pokemon[req.params.indexOfPokemonArray].img = req.body.img;
+    pokemon[req.params.indexOfPokemonArray].type = req.body.type;
+    pokemon[req.params.indexOfPokemonArray].stats.hp = req.body.hp;
+    pokemon[req.params.indexOfPokemonArray].stats.attack = req.body.attack;
+    pokemon[eq.params.indexOfPokemonArray].stats.defence = req.body.defense;
+    pokemon[req.params.indexOfPokemonArray].stats.spattack = req.body.spattack;
+    pokemon[req.params.indexOfPokemonArray].stats.spdefense = req.body.spdefense;
+    pokemon[req.params.indexOfPokemonArray].stats.speed = req.body.speed;
+    pokemon[req.params.indexOfPokemonArray] = req.body
   res.redirect("/pokemon")
 });
 
 //CREATE
 app.post("/pokemon", (req, res)=>{
-  pokemon.push(req.body)
+  pokemon.push({
+    name: req.body.name,
+    image: req.body.img,
+    type: [req.body.type],
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.attack,
+      defense: req.body.defense,
+      spattack: req.body.spattack,
+      spdefense: req.body.spdefense,
+      speed: req.body.speed
+    }
+  })
+
   res.redirect("/pokemon")
 });
 
 //EDIT
+app.get("/pokemon/:indexOfPokemonArray/edit", (req, res) => {
+  res.render("edit.ejs", {
+    name: pokemon[req.params.indexOfPokemonArray].name,
+    img: pokemon[req.params.indexOfPokemonArray].img,
+    type: pokemon[req.params.indexOfPokemonArray].type,
+    hp: pokemon[req.params.indexOfPokemonArray].stats.hp,
+    attack: pokemon[req.params.indexOfPokemonArray].stats.attack,
+    defense: pokemon[req.params.indexOfPokemonArray].stats.defense,
+    spattack: pokemon[req.params.indexOfPokemonArray].stats.spattack,
+    spdefense: pokemon[req.params.indexOfPokemonArray].stats.spdefense,
+    speed: pokemon[req.params.indexOfPokemonArray].stats.speed,
+    index: req.params.indexOfPokemonArray
+  });
+});
 
 //SHOW
 app.get('/pokemon/:id', (req, res) => {
